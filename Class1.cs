cdcdf4ee0340a410dd27688fa536122f505d9664
Женя Безкуцький <bezkuczkij@gmail.com>
2024-08-20 17:54:39 +0300

    dew2343

--- Class1.cs
+++ Class1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace Laba_1
 {
-    internal class Program
+    internal class Class1
     {
-        static void Main(string[] args)
-        {
-            int n = 5;
-        }
     }
 }
