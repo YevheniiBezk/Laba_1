25db6142829eb368a778b3cccd03977e3a55f717
Женя Безкуцький <bezkuczkij@gmail.com>
2024-08-20 18:33:46 +0300

    LABA1

--- Class1.cs
+++ Class1.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Laba_1
 {
-    internal class Class1
+    class Computer
+{
+    public string Brand;
+    public string Model;
+    public string Processor;
+    public int RAM; 
+    public int Storage; 
+    public double Price; 
+    public bool HasDedicatedGPU;
+    public bool HasTouchscreen;
+
+    public double GetPricePerGB()
     {
+        return Price / (RAM + Storage);
     }
+}
 }
--- Program.cs
+++ Program.cs
@@ -5,12 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Laba_1
+{ 
+
+    class Program
 {
-    internal class Program
+    static void Main(string[] args)
+
     {
-        static void Main(string[] args)
-        {
-            int n = 5;
-        }
+      Console.OutputEncoding = System.Text.Encoding.UTF8;
+            
+            Console.Write("Введіть бренд комп'ютера: ");
+        string brand = Console.ReadLine();
+
+        Console.Write("Введіть модель комп'ютера: ");
+        string model = Console.ReadLine();
+
+        Console.Write("Введіть процесор: ");
+        string processor = Console.ReadLine();
+
+        Console.Write("Введіть об'єм оперативної пам'яті (ГБ): ");
+        string sRAM = Console.ReadLine();
+
+        Console.Write("Введіть об'єм накопичувача (ГБ): ");
+        string sStorage = Console.ReadLine();
+
+        Console.Write("Введіть ціну (у доларах): ");
+        string sPrice = Console.ReadLine();
+
+        Console.Write("Чи має комп'ютер дискретну відеокарту? (у-так, н-ні): ");
+        ConsoleKeyInfo keyHasDedicatedGPU = Console.ReadKey();
+        Console.WriteLine();
+
+        Console.Write("Чи має комп'ютер сенсорний екран? (у-так, н-ні): ");
+        ConsoleKeyInfo keyHasTouchscreen = Console.ReadKey();
+        Console.WriteLine();
+
+        Computer myComputer = new Computer();
+        myComputer.Brand = brand;
+        myComputer.Model = model;
+        myComputer.Processor = processor;
+        myComputer.RAM = int.Parse(sRAM);
+        myComputer.Storage = int.Parse(sStorage);
+        myComputer.Price = double.Parse(sPrice);
+        myComputer.HasDedicatedGPU = keyHasDedicatedGPU.Key == ConsoleKey.Y ? true : false;
+        myComputer.HasTouchscreen = keyHasTouchscreen.Key == ConsoleKey.Y ? true : false;
+
+        double pricePerGB = myComputer.GetPricePerGB();
+
+        Console.WriteLine();
+        Console.WriteLine("Дані про комп'ютер:");
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Бренд: " + myComputer.Brand);
+        Console.WriteLine("Модель: " + myComputer.Model);
+        Console.WriteLine("Процесор: " + myComputer.Processor);
+        Console.WriteLine("Оперативна пам'ять: " + myComputer.RAM + " ГБ");
+        Console.WriteLine("Накопичувач: " + myComputer.Storage + " ГБ");
+        Console.WriteLine("Ціна: $" + myComputer.Price.ToString("0.00"));
+        Console.WriteLine(myComputer.HasDedicatedGPU ? "Має дискретну відеокарту" : "Не має дискретної відеокарти");
+        Console.WriteLine(myComputer.HasTouchscreen ? "Має сенсорний екран" : "Не має сенсорного екрану");
+        Console.WriteLine();
+        Console.WriteLine("Ціна за ГБ: $" + pricePerGB.ToString("0.00"));
+        Console.ReadKey();
     }
+}
 }
